d86e6bd21beba95f0950612e9ce389df2cb953a0
Rory Duncan <robust.rory@gmail.com>
2024-04-14 17:00:37 -0700

    Fix feed endpoint

--- src/lib/firebase/firebase-cert.ts
+++ src/lib/firebase/firebase-cert.ts
@@ -1,19 +1,24 @@
 import { cert } from "firebase-admin/app";
+import {
+	FIREBASE_PRIVATE_KEY,
+	FIREBASE_CLIENT_EMAIL,
+	FIREBASE_PROJECT_ID
+} from "$env/static/private";
 
-if (!process.env.FIREBASE_PRIVATE_KEY) {
+if (!FIREBASE_PRIVATE_KEY) {
 	throw new Error(".env: FIREBASE_PRIVATE_KEY is missing");
 }
 
-if (!process.env.FIREBASE_CLIENT_EMAIL) {
+if (!FIREBASE_CLIENT_EMAIL) {
 	throw new Error(".env: FIREBASE_CLIENT_EMAIL is missing");
 }
 
-if (!process.env.FIREBASE_PROJECT_ID) {
+if (!FIREBASE_PROJECT_ID) {
 	throw new Error(".env: FIREBASE_PROJECT_ID is missing");
 }
 
 export default cert({
-	privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-	clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-	projectId: process.env.FIREBASE_PROJECT_ID
+	privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+	clientEmail: FIREBASE_CLIENT_EMAIL,
+	projectId: FIREBASE_PROJECT_ID
 });
